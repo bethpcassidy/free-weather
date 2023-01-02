7ecfbeb053d880b9c3ddcfe1aeef316a10fdaa99
beth <bethcassdy@gmail.com>
2023-01-02 15:20:52 -0500

    views, jb, and log entry 1

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,11 +1,29 @@
 class ForecastsController < ApplicationController
   def index
     @forecasts = Forecast.all
-    render json: @forecasts
+    render :index
   end
 
   def show
     @forecast = Forecast.find_by(id: params[:id])
-    render json: @forecast
+    render :show
+  end
+
+  def edit
+    @forescast = Forescast.find_by(id: params[:id])
+    render template: "forescasts/edit"
+  end
+
+  def update
+    @forescast = forescast.find_by(id: params[:id])
+    @forescast.high = params[:forescast][:high]
+    @forescast.low = params[:forescast][:low]
+    @forecast.average = params[:forecast][:average]
+    @forecast.body = params[:forecast][:body]
+    @forecast.image = params[:forecast][:image]
+    @forecast.zipcode = params[:forecast][:zipcode]
+
+    @forecast.save
+    redirect_to "/forecasts"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "forecasts#index"
+
   resources :forecasts
 end
